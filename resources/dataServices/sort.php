0e0269623fa9dd20a1796aa9d1ad342e87dbeea1
AL DYNAK <adynak@gmail.com>
2020-04-07 13:03:42 -0700

    back nine now

--- resources/dataServices/sort.php
+++ resources/dataServices/sort.php
@@ -1,46 +1,33 @@
 <?php
 
-$data = array (
- 	array( "Producer" => "Zerba",
+function sortWines($data){
+
+  foreach ($data as $key => $row) {
+    $vintage[$key]      = $row['Vintage'];
+    $producer[$key]     = $row['Producer'];
+    $varietal[$key]     = $row['Varietal'];
+  }
+
+  array_multisort($producer, SORT_ASC, $varietal, SORT_ASC, $vintage, SORT_DESC, $data );
+  return $data;
+
+}
+
+$wine = array (
+  array( "Producer" => "Zerba",
                "Varietal" => "Pinot Noir",
                "Vintage" => 2016,
                "return_fare" => 300),
- 	array( "Producer" => "Zerba",
+  array( "Producer" => "Zerba",
                "Varietal" => "Pinot Noir",
                "Vintage" => 2015,
                "return_fare" => 350),
- 	array( "Producer" => "Zerba",
+  array( "Producer" => "Zerba",
                "Varietal" => "Pinot Noir",
                "Vintage" => 2016,
                "return_fare" => 340)
 );
-echo json_encode($data);
-echo "<br><br>";
 
-
-foreach ($data as $key => $row) {
-	echo $key . "<br>";
-    $vintage[$key]      = $row['Vintage'];
-    $producer[$key]     = $row['Producer'];
-    $varietal[$key]     = $row['Varietal'];
-}
-
-print_r($producer);
-echo "<br><br>";
-
-print_r($varietal);
-echo "<br><br>";
-
-print_r($vintage);
-echo "<br><br>";
-
-
-array_multisort($producer, SORT_ASC, $varietal, SORT_ASC, $vintage, SORT_DESC, $data );
-
-
-echo "<br><br>";
-
-echo json_encode($data);
-
-
+$wine = sortwines($wine);
+echo "<br>".json_encode($wine);
 ?>
--- resources/dataServices/test.php
+++ resources/dataServices/test.php
@@ -4,53 +4,20 @@ $appArray['producers'];
 
 $json = '{"producers":[{"name":"Zerba","isExpanded":false,"wines":[{"vintage":"2014","varietal":"Syrah","designation":"Estate","ava":"Walla Walla","storageBins":[{"binName":"A","bottleCount":1},{"binName":"B","bottleCount":2}]},{"vintage":"2013","varietal":"Grenache","designation":"The Rocks","ava":"Walla Walla","storageBins":[{"binName":"A","bottleCount":3},{"binName":"B","bottleCount":4}]},{"vintage":"2012","varietal":"Grenache","designation":"Estate","ava":"Red Hills","storageBins":[{"binName":"A","bottleCount":5},{"binName":"B","bottleCount":6}]}]},{"name":"Del Rio","isExpanded":false,"wines":[{"vintage":"2011","varietal":"Syrah","designation":"Estate","ava":"Umpqua Valley","storageBins":[{"binName":"A","bottleCount":7},{"binName":"B","bottleCount":8}]},{"vintage":"2010","varietal":"Grenache","designation":"Lot 24","ava":"Umpqua Valley","storageBins":[{"binName":"A","bottleCount":9},{"binName":"B","bottleCount":10},{"binName":"Offsite","bottleCount":12}]}]}]}';
 
-$wineArray = json_decode($json,true);
-// print_r($wineArray);
-// die();
-$producers = $wineArray["producers"];
 
-foreach ($producers as $key => $value) {
-    // echo $value["name"] . "<br>";
-    // echo ($value["isExpanded"]== true ? "true" : "false") . "<br>";
-    // print_r($value["wines"]) . "<br>";
-}
-
-echo "<br>";
-
+    $br = "<br>";
     $index = -1;
-    $tempArray = array();
-    $csv = array();
-    $url = '/Library/WebServer/Documents/angular/git/Wine/resources/data/bottles1.csv';
+    $producers = array();
+    $iWines = array();
 
-    $rows = array_map('str_getcsv', file($url));
-    $header = array_shift($rows);
+    $url = '/Library/WebServer/Documents/Wine/resources/data/bottles1.csv';
+    $csv = csvDecode($url);
+    $csv = sortWines($csv);
     
-    foreach ($rows as $row) {
-        $csv[] = array_combine($header, $row);
-    }
-
-    $firstRow['Producer'] = $csv[0]["Producer"];
-
-    $storageBin = array("location"    => $csv[0]['Location'],
-                        "bin"         => $csv[0]['Bin'],
-                        "bottleCount" => "1");
-
-    $bottle = array("iWine"           => $csv[0]['iWine'],
-                    "vintage"         => $csv[0]['Vintage'],
-                    "varietal"        => $csv[0]['Varietal'],
-                    "designation"     => $csv[0]['Designation'],
-                    "ava"             => $csv[0]['Appellation'],
-                    "storageBins"     => $storageBin);
-    
-    // $appArray['producers'][] = array("name"       => $firstRow['Producer'], 
-    //                                  "isExpanded" => false,
-    //                                  "wines"      => $bottle);
-
-    // array_push($tempArray, $firstRow['Producer']);
-
     foreach ($csv as $key => $value) {
 
-        if (!in_array($value["Producer"], $tempArray)){
+        if (!in_array($value["Producer"], $producers)){
+            // new ["Producer"]
             $storageBin = array("location"    => $value['Location'],
                                 "bin"         => $value['Bin'],
                                 "bottleCount" => "1");
@@ -66,8 +33,11 @@ echo "<br>";
                                              "isExpanded" => false,
                                              "wines"      => $bottle);
 
-            array_push($tempArray, $value["Producer"]);
-	   } else {
+            array_push($producers, $value["Producer"]);
+            array_push($iWines, $value["iWine"]);
+
+       } else {
+            // existing ["Producer"]
             $producer = $value['Producer'];
 
             $storageBin = array("location"    => $value['Location'],
@@ -83,27 +53,40 @@ echo "<br>";
 
 
             $foundAt = -1;
-            foreach ($appArray["producers"] as $key => $value) {
+            foreach ($appArray["producers"] as $key => $item) {
                 $foundAt ++;
-                if ($value['name'] == $producer){
+                if ($item['name'] == $producer){
                     break;
                 }
             }
 
-            $temp = array();
-            $previousWines = $appArray['producers'][$foundAt]['wines'];
-            array_push($temp, $previousWines);
-            array_push($temp, $bottle);
-            unset($appArray['producers'][$foundAt]['wines']);
+            // echo $appArray['producers'][$foundAt]['wines']['iWine'];
 
-            $appArray['producers'][$foundAt]['wines'][] = $temp;
+            if (!in_array($value["iWine"], $iWines)){
+                // append a new wine for this producer
+                $temp = array();
+                $previousWines = $appArray['producers'][$foundAt]['wines'];
+                array_push($temp, $previousWines);
+                array_push($temp, $bottle);
+                unset($appArray['producers'][$foundAt]['wines']);
+                $appArray['producers'][$foundAt]['wines'][] = $temp;
+                array_push($iWines, $value["iWine"]);
+            } else {
+                // append a new storage location for this wine
+                $temp = array();
+                $previousbins = $appArray['producers'][$foundAt]['wines']['storageBins'];
+                array_push($temp, $previousbins);
+                array_push($temp, $storageBin);
+                unset($appArray['producers'][$foundAt]['wines']['storageBins']);
+                $appArray['producers'][$foundAt]['wines']['storageBins'] = $temp;
+            }
 
        }
 
     }
 
-
-    echo json_encode(($appArray));
+    $json = json_encode($appArray);
+    echo $json;
     die();
 
 $producers = $appArray["producers"];
@@ -117,10 +100,22 @@ foreach ($producers as $key => $value) {
     echo "<br>";
 }
 
-// $miso = csvToJson('/Library/WebServer/Documents/angular/git/Wine/resources/data/bottles.csv');
-echo "<br>";
 
-// print_r($miso);
+
+
+function sortWines($data){
+// param $data is an array
+  
+  foreach ($data as $key => $row) {
+    $vintage[$key]      = $row['Vintage'];
+    $producer[$key]     = $row['Producer'];
+    $varietal[$key]     = $row['Varietal'];
+  }
+
+  array_multisort($producer, SORT_ASC, $varietal, SORT_ASC, $vintage, SORT_DESC, $data );
+  return $data;
+
+}
 
 
 function csvToJson($fname) {
@@ -128,12 +123,12 @@ function csvToJson($fname) {
     if (!($fp = fopen($fname, 'r'))) {
         die("Can't open file...");
     } else {
-    	// echo "reading $fname";
+        // echo "reading $fname";
     }
     echo "<br>";
 
     $index = -1;
-	$stopAfter = 8;
+    $stopAfter = 8;
     
     //read csv headers
     $key = fgetcsv($fp,"1024",",");
@@ -141,16 +136,16 @@ function csvToJson($fname) {
     // parse csv rows into array
     $json = array();
         while ($row = fgetcsv($fp,0,",")) {
-        	$index ++;
-   //      	if ($index > 6){
-        		$line = array_combine($key, $row);
-        		echo json_encode($line);
-        		$json[] = $line;
-   //      	}
-        	if ($index == $stopAfter){
-				break;
-			}
-			echo  "<br>";
+            $index ++;
+   //       if ($index > 6){
+                $line = array_combine($key, $row);
+                echo json_encode($line);
+                $json[] = $line;
+   //       }
+            if ($index == $stopAfter){
+                break;
+            }
+            echo  "<br>";
     }
     
     // release file handle
@@ -160,5 +155,21 @@ function csvToJson($fname) {
     return json_encode($json);
 }
 
+function csvDecode($url) {
+    $csv = array();
+    
+    $rows = array_map('str_getcsv', file($url));
+    $header = array_shift($rows);
+    
+    foreach ($rows as $row) {
+        // foreach ($row as $key=>$value){
+        //     $row[$key] = utf8_encode($value);
+        //     $row[$key] = utf8_decode($value);
+        // }
+        $csv[] = array_combine($header, $row);
+    }
+
+    return $csv;
+}
  
 ?>
